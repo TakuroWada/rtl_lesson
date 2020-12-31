68b5fbeed25a52a02896ce6be7198b8d93ed481d
Takuro Wada <takuro.wada6@gmail.com>
2020-12-31 15:31:50 +0900

    テスト対象のコンポーネント作成

---
+++ src/Render.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Render = (props) => {
+  return (
+    <div>
+      <h1>React Testing Library Lesson</h1>
+      <input type="text" />
+      <button>Click1</button>
+      <button>Click2</button>
+      <p>test</p>
+      <span>@React</span>
+    </div>
+  );
+};
+
+Render.propTypes = {};
+
+export default Render;
